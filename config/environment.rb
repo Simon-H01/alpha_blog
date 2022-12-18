7b0905647a9bc2fbd9466297622bd45bc709b87d
Simon Höfling <simon@hoeflia.eu>
2022-12-18 16:52:29 +0100

    Styling the article view page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# This is for getting rid of the rails auto generatet error field if you don´t complete a form
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
