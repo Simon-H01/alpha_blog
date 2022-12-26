d4c85b7a9fa10af1f1987fc7f5eb17df1fc44d71
Simon Höfling <simon@hoeflia.eu>
2022-12-26 19:31:48 +0100

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,42 @@
 class UsersController < ApplicationController
 
-    def new
-        @user = User.new
-    end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  def new
+      @user = User.new
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your account information was successfully updated"
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
         redirect_to articles_path
       else
-        render :edit, status: 422
+          render :new, status: 422
       end
     end
+  
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-          redirect_to articles_path
-        else
-            render :new, status: 422
-        end
-      end
-    
-      private
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+    private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 
 end
